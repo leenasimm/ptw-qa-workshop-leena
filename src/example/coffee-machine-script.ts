e1e94962781f371bab7010ab1da4022703d7cfee
Leena Simm <leena.simm@inbank.ee>
2024-03-13 14:23:43 +0200

    DEVX-113 | added coffee script functionality

--- src/example/coffee-machine-script.ts
+++ src/example/coffee-machine-script.ts
@@ -1,36 +1,36 @@
 // Ex. 1: declare variables and use them
-let MilkAmount: number = 50
+let milkAmount: number = 50
 let coffeeAmount: number = 100
 let hasSyrup: boolean = true
 let hasIce: boolean = true
 
-console.log(`Preparing coffee ${coffeeAmount} ml with ${MilkAmount} ml of milk`)
+console.log(`Preparing coffee ${coffeeAmount} ml with ${milkAmount} ml of milk`)
 
 // Ex. 1.1: declare const variable and demonstrate
-MilkAmount = 150 // works ok, because let
+milkAmount = 150 // works ok, because let
 const waterAmount: number = 100
 //waterAmount = 200 // does not work, because const
 
 
 // Ex. 2: create conditional statements
 if (hasSyrup && hasIce) {
-  console.log(`Preparing iced coffee ${coffeeAmount} ml with ${MilkAmount} ml with syrup`)
+  console.log(`Preparing iced coffee ${coffeeAmount} ml with ${milkAmount} ml with syrup`)
 } else if (hasIce) {
-  console.log(`Preparing iced coffee ${coffeeAmount} ml with ${MilkAmount} ml`)
+  console.log(`Preparing iced coffee ${coffeeAmount} ml with ${milkAmount} ml`)
 } else if (hasSyrup) {
-  console.log(`Preparing coffee ${coffeeAmount} ml with ${MilkAmount} ml with syrup`)
+  console.log(`Preparing coffee ${coffeeAmount} ml with ${milkAmount} ml with syrup`)
 }
 
 
 let coffeeTotalAmount: number = 150
-if (MilkAmount + coffeeAmount > coffeeTotalAmount) {
+if (milkAmount + coffeeAmount > coffeeTotalAmount) {
   console.log("Cannot make coffee, total amount is less that requested ingredients!")
 } else {
   console.log("All good, making your coffee!")
 }
 
-MilkAmount = 100
-if (MilkAmount + coffeeAmount > coffeeTotalAmount) {
+milkAmount = 100
+if (milkAmount + coffeeAmount > coffeeTotalAmount) {
   console.log("Cannot make coffee, total amount is less that requested ingredients!")
 } else {
   console.log("All good, making your coffee!")
@@ -39,7 +39,7 @@ if (MilkAmount + coffeeAmount > coffeeTotalAmount) {
 // Ex. 3: create loops
 const numOfOrders: number = 5
 for (let i = 1; i <= numOfOrders; i++) {
-  console.log(`Preparing coffee nr. ${i} ${coffeeAmount} ml with ${MilkAmount} ml of milk`)
+  console.log(`Preparing coffee nr. ${i} ${coffeeAmount} ml with ${milkAmount} ml of milk`)
 }
 
 const coffeeAmounts: number[] = [100, 150, 200]
